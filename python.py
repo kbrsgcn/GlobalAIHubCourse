26c5276f260a4242059fd4ad227ff088e336ec0f
kbrsgcn <109732663+kbrsgcn@users.noreply.github.com>
2024-02-06 23:44:18 +0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,4 @@
+age=int(input("Please enter your age: "))
+print("Your age after five years will be ", age + 5 )
+momAge=int(input("Please enter your mom's age "))
+print("Your mom's age after 5 years will be ", momAge + 5, "The age gap between you and your mom is didn't change tho")
